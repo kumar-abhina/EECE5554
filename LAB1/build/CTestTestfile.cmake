9b7e1f06d17e75e76e4e14e410a011f12f1085b1
kumar-abhina <kumar.abhina@northeastern.edu>
2023-02-06 21:45:31 -0500

    Lab 1 upload

--- LAB1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abhinav/catkin_ws/src/LAB1/src
-# Build directory: /home/abhinav/catkin_ws/src/LAB1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("gps_driver")
